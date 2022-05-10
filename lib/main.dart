2081c8e05d513f668f68a5fa545a108e42854605
cchampou <clement@champouillon.com>
2022-05-10 21:12:56 +0200

    feat: add firebase analytics

--- lib/main.dart
+++ lib/main.dart
@@ -8,8 +8,11 @@ import 'settings/settings_page.dart';
 import 'package:provider/provider.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
+import 'package:firebase_analytics/firebase_analytics.dart';
 
 import 'settings_provider.dart';
+
+FirebaseAnalytics analytics = FirebaseAnalytics.instance;
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
